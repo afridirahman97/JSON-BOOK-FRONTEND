014a08464a1679d4441f1e416b60761a01c1ce0a
afridirahman97 <afridirahman97@gmail.com>
2023-03-19 16:23:50 +0600

    frontend fixes

--- src/app/view-responses.service.ts
+++ src/app/view-responses.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ViewResponsesService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   // get all responses
-  getData(){
-    let url="http://localhost:8080/responses";
-    return this.http.get(url);
- }
- // responses by id
- getResponseById(responsesId:number){
-  let url="http://localhost:8080/responses/"+responsesId.toString();
-  return this.http.get(url);
+  getData() {
+    
+    let getURL = this.router.url
+    console.log(getURL)
+    const segments = getURL.split('/');
+    const id = segments[segments.length - 1];
+    console.log(id)
 
-}
+    let url = `http://localhost:8080/responses/response/${id}`;
+    return this.http.get(url);
+  }
+  // responses by id
+  getResponseById(responsesId: number) {
+    let url = "http://localhost:8080/responses/" + responsesId.toString();
+    return this.http.get(url);
+
+  }
 }
