476c7e73f68642885815c16b991ce08303c0327e
taareek <tarekiub17@gmail.com>
2023-03-23 18:03:23 +0600

    worked on searching

--- src/app/groups/groups.component.ts
+++ src/app/groups/groups.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
 import {NavigationExtras, Router} from '@angular/router';
 import { GroupDataService } from '../group-data.service';
 import {DeleteGroupService} from "../delete-group.service";
+import { FormGroup, FormControl } from '@angular/forms';
 import Swal from 'sweetalert2';
 //importing necessary font awesome emojis for group component
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'; 
-import { faPlusSquare, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'; 
+import { faPlusSquare, faMagnifyingGlass, faEye } from '@fortawesome/free-solid-svg-icons'; 
+import { Pass } from 'codemirror';
 
 
 @Component({
@@ -15,7 +17,7 @@ import { faPlusSquare, faMagnifyingGlass } from '@fortawesome/free-solid-svg-ico
 })
 export class GroupsComponent {
 
-
+   groupNameSearch : string = '';
    topSection: "topSection" = "topSection";
    tabButtonPadding: "tabButtonPadding" = "tabButtonPadding";
    navButton: "navButton" = "navButton";
@@ -24,19 +26,34 @@ export class GroupsComponent {
    faCoffee = faCoffee;
    faPlusSquare = faPlusSquare;
    faMagnifyingGlass = faMagnifyingGlass;
-
+   group_name : string = "";
+  //  search_data : constant;
 
   constructor(private router: Router, private data: GroupDataService,private del:DeleteGroupService) {
     this.data.getData().subscribe(data =>{
       //console.warn(data)
-      this.rows = data
-      //console.log(this.rows)
+     
+      this.rows = data;
+      // this.group_name = this.rows.groupName
+      console.log(this.rows)
+      // console.log("data" + data)
+      // console.log(typeof this.rows)
+      // console.log("search value: " + this.groupNameSearch)
+      
+      // for (const k in this.rows) {
+      //   const v = this.rows[k];
+      //   console.log(v.groupName);
+      // }
 
+      // console.log("search results from constructot: " + this.groupNameSearch)
+      
     })
+
+  
   }
 
   headers = ["Group Name", "Actions"];
-
+  
 
 
 
@@ -94,6 +111,31 @@ export class GroupsComponent {
 
   performCreate() {
     this.router.navigate(['/group/create'])
+  }
+
+  performSearch(){
+    if(this.groupNameSearch != ""){
+      console.log(this.groupNameSearch)
+    }else{
+      console.log("you did't search any group !")
+    }
+
+    this.data.getData()
+    for (const k in this.rows) {
+      const row = this.rows[k];
+      console.log("from row: "+row.groupName)
+      if (row.groupName == this.groupNameSearch){
+        // this.search_data =  row;
+        // const search_data = this.rows[k]
+        // console.log("Hola: "+search_data)
+        // console.log("gjkfr: "+ search_data[0])
+        this.rows = row;
+        this.router.navigate(['/groups/'])
+      }
+      // console.log(k + " value of k");
+      console.log(row.groupName);
+    }
+    
   }
 }
 
